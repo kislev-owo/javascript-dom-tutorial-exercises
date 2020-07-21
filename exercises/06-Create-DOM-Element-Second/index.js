42a4da507c0b5f8d1d7521d81dbef2d62725c9e0
kislev-owo <kislev_083@hotmail.com>
2020-07-21 01:01:36 +0000

    06 ejercicio

--- exercises/06-Create-DOM-Element-Second/index.js
+++ exercises/06-Create-DOM-Element-Second/index.js
@@ -1,3 +1,6 @@
 //you code here
 document.body.innerHTML = "<h1>Hello World</h1>";
 
+document.body.innerHTML = `<img src="http://placehold.it/350x150">`;
+
+// escribimos una direccion interna al HTML donde ponemos una imagen
