32f84cf0a05425afd93e00d0791a34e00afcb905
kislev-owo <kislev_083@hotmail.com>
2020-07-21 03:39:53 +0000

    ejercicio 5 ressuelto

--- exercises/05-Create-DOM-Element-First/index.js
+++ exercises/05-Create-DOM-Element-First/index.js
@@ -4,3 +4,8 @@ document.querySelector("#myDiv").appendChild(elem);
 
 //Your code here
 // Don'change the code above
+let elemp = document.createElement("P");
+elemp.innerHTML = "Hello World";
+elemp.style.background = "yellow";
+document.querySelector("#myDiv").appendChild(elemp);
+
