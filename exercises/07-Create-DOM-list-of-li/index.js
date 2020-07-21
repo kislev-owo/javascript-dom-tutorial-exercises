eec70941255753e3d1dae7f8599be7704059fabe
kislev-owo <kislev_083@hotmail.com>
2020-07-21 01:21:00 +0000

    ejercicio 07

--- exercises/07-Create-DOM-list-of-li/index.js
+++ exercises/07-Create-DOM-list-of-li/index.js
@@ -1,7 +1,12 @@
 let beginning = "<ul>";
-let liststring = `<li>First Item</li><li>Second Item</li><li>Third Item</li>
-`;
+
+let liststring = ``;
+
 let ending = "</ul>";
+
+liststring = `<li>First Item</li>
+             <li>Second Item</li>   
+             <li>Third Item</li>`; // creamos la variable liststring y adentro                          ponemos las listas una terminando la otra
 
 // do not modify after this line
 document.querySelector("#myDiv").innerHTML = beginning + liststring + ending;
